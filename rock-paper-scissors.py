6fb28446785b0fc1969f3baa40967c0265e8dfdf
itwasntzak_window <zakary100@gmail.com>
2020-01-03 01:23:24 -0700

    making recommend changed from reddit post

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -7,92 +7,109 @@ from time import sleep
 if os.name == 'nt':
     import msvcrt
 
-os.system('cls' if os.name == 'nt' else 'clear')
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def wrong_input():
+    print('\nInvalid input...')
+    sleep(3)
+    clear_screen()
 
 
 def play():
     options = ('rock', 'paper', 'scissor')
     while True:
-        player_option = int(input('CHOOSE A WEAPON WITH WHICH TO DO BATTLE!\n'
-                                  '(1. rock, 2. paper, 3. scissor)\n'))
-        random_number = randrange(3)
-
-        if player_option == 1:
-            print('\nROCK')
-        elif player_option == 2:
-            print('\nPAPER')
-        elif player_option == 3:
-            print('\nSCISSOR')
+        try:
+            player_choice = int(input(
+                'CHOOSE A WEAPON WITH WHICH TO DO BATTLE!\n'
+                '(1. rock, 2. paper, 3. scissor)\n'))
+        except ValueError:
+            wrong_input()
+            continue
+        if player_choice == 0:
+            wrong_input()
+            continue
         else:
-            print('\nInvalid input...\n')
-        sleep(1)
-        print('\n.VS\n')
-        sleep(2)
-        print(options[random_number].upper())
-        sleep(2)
-        results(player_option, random_number)
-        sleep(2)
-        os.system('cls' if os.name == 'nt' else 'clear')
-        try_again()
+            try:
+                player_choice = options[player_choice - 1]
+            except IndexError:
+                wrong_input()
+                continue
+            computer_choice = options[randrange(len(options))]
+            clear_screen()
+            print('\n' + player_choice.upper())
+            sleep(2)
+            print('\n.vs'.upper())
+            sleep(2)
+            print('\n' + computer_choice.upper())
+            sleep(2)
+            results(player_choice, computer_choice)
+            sleep(4)
+            try_again()
 
 
-def results(player_option, random_number):
-    if (player_option == 1 and random_number == 2)\
-            or (player_option == 2 and random_number == 0)\
-            or (player_option == 3 and random_number == 1):
+def results(player_choice, computer_choice):
+    if (player_choice, computer_choice) in\
+            (('rock', 'scissor'), ('paper', 'rock'), ('scissor', 'paper')):
         print('\nWINNIN\'! WINNIN\'! CHICKEN DICKIN\'!!!')
 
-    elif (player_option == 1 and random_number == 1)\
-            or (player_option == 2 and random_number == 2)\
-            or (player_option == 3 and random_number == 0):
+    elif (player_choice, computer_choice) in\
+            (('rock', 'paper'), ('paper', 'scissor'), ('scissor', 'rock')):
         print('\nLOSER! TRY AGAIN!')
 
-    elif (player_option == 1 and random_number == 0)\
-            or (player_option == 2 and random_number == 1)\
-            or (player_option == 3 and random_number == 2):
+    elif (player_choice, computer_choice) in\
+            (('rock', 'rock'), ('paper', 'paper'), ('scissor', 'scissor')):
         print('\nANTICLIMACTIC! IT\'S A DRAW! TRY AGAIN!')
 
 
 def try_again():
-    print('Would you like to play again?\nPress enter for yes, escape for no')
-    if os.name == 'nt':
-        option = ord(msvcrt.getch())
-        os.system('cls')
-        if option == 13:
-            pass
-        elif option == 27:
-            exit()
+    clear_screen()
+    while True:
+        print('Would you like to play again?\n\n'
+              'Press enter for yes, escape for no')
+        if os.name == 'nt':
+            option = ord(msvcrt.getch())
+            if option == 13:
+                clear_screen()
+                break
+            elif option == 27:
+                exit()
+            else:
+                wrong_input()
+                continue
         else:
-            print('Invalid input...')
-    else:
-        option = ord(sys.stdin.read(1))
-        os.system('clear')
-        if option == 10:
-            pass
-        elif option == 27:
-            exit()
-        else:
-            print('Invalid input...')
+            option = ord(sys.stdin.read(1))
+            if option == 10:
+                clear_screen()
+                break
+            elif option == 27:
+                exit()
+            else:
+                wrong_input()
+                continue
 
 
 while True:
+    clear_screen()
     print('WELCOME! WELCOME! WELCOME!\n\nto\n\nROCK PAPER SCISSORS!\n\n'
           'Press enter to play or escape to close the game')
     if os.name == 'nt':
         option = ord(msvcrt.getch())
-        os.system('cls')
         if option == 13:
+            clear_screen()
             play()
         elif option == 27:
             exit()
         else:
-            print('Invalid input...')
+            wrong_input()
     else:
         option = ord(sys.stdin.read(1))
-        os.system('clear')
         if option == 10:
+            clear_screen()
             play()
         elif option == 27:
             exit()
         else:
-            print('Invalid input...')
+            wrong_input()
